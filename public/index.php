86b0f10d81c5c6e102af927ba594d8aa1a49c4d6
Katalam <goerss99@gmail.com>
2020-06-20 18:10:34 +0200

    Bugfix copy to clipboard, moved to clipboard.js libary

--- public/index.php
+++ public/index.php
@@ -14,24 +14,22 @@
             <input class="button" type="submit" name="submit" value="&#xf105;">
         </form>
         <p id="error"></p>
+        <form>
+            <input class="input" id="redirect_input" value="">
+            <input class="btn button" type="submit" name="" value="&#xf0c5;" data-clipboard-target="#redirect_input">
+        </form>
     </div>
 </body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.4/clipboard.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/js/all.min.js"></script>
 <script
   src="https://code.jquery.com/jquery-3.5.1.min.js"
   integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
   crossorigin="anonymous"></script>
 <script type="text/javascript">
-function copyToClipboard(str) {
-  const el = document.createElement('textarea')
-  el.value = str
-  el.setAttribute('readonly', '')
-  el.style.position = 'absolute'
-  el.style.left = '-9999px'
-  document.body.appendChild(el)
-  el.select()
-  document.execCommand('copy')
-  document.body.removeChild(el)
+new ClipboardJS('.btn');
+function paste(str) {
+  $('#redirect_input')[0].setAttribute('value', str)
 }
 
 function errorMsg(msg) {
@@ -55,6 +53,7 @@ if (isset($_GET['slug'])) {
 }
 if (isset($_POST['submit'])) {
     $url = $_POST['input_url'];
+    if ($url == '') return;
     if (filter_var($url, FILTER_VALIDATE_URL) === false) {
         echo '<script type="text/javascript">',
         `errorMsg('Not a valid url.');`,
@@ -63,7 +62,7 @@ if (isset($_POST['submit'])) {
     }
     $slug = store($url);
     echo '<script type="text/javascript">',
-    'copyToClipboard(' . $slug . ');',
+    'paste("' . $_ENV['DOMAIN'] . '/?slug=' . $slug . '");',
     '</script>';
 }
 ?>
